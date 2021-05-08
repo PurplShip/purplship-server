da44c098ff058ee937621e4484fa51671d7258e8
Dan <3097027-DanH91@users.noreply.gitlab.com>
2021-05-08 12:28:49 -0400

    introduce shipment and tracking status details

--- apps/client/purpleserver/client/views/app.py
+++ apps/client/purpleserver/client/views/app.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('api_logs/<str:log_id>', index, name='api_logs'),
     path('buy_label/<str:id>', index, name='buy_label'),
     path('configurations/<str:config_name>', index, name='configurations'),
+    path('shipments/<str:shipment_id>', index, name='shipment_details'),
     path('trackers', index, name='trackers'),
     path('settings/<str:settings_name>', index, name='settings'),
 ]
